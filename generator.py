e74cb4ac376a82f4e5092067b3ca62148b599589
Darshan Raul <dar.raul@gmail.com>
2019-03-27 17:10:19 +0530

    sample template generated

--- generator.py
+++ generator.py
@@ -1,39 +1,61 @@
-
-
 import json
-#####################Sample Template Anatomy##################
-template='''{
+
+def lambda_handler(event, context):
+    # TODO implement
+    template='''{
     "AWSTemplateFormatVersion" : "2010-09-09",
   
-    "Description" : "AWS CloudFormation Template",
+    "Description" : "AWS CloudFormation Template Description",
   
    
   
     "Resources" : {
         
     }
-  }'''
-  
-########################  
-s3type="AWS::S3::Bucket"
-stack=json.loads(template)
-resources=stack["Resources"] 
-s3bucket={}
-s3bucket.update({"Type" : s3type})
+    }'''
 
-s3bucket.update({
-   "Properties" :{
-        "AccessControl" : "PublicRead",
-       "BucketName" : "dfdfdfdfdfgguyigu"  
-   }
-})
-#s3bucketproperties={}
 
-#s3bucketproperties["AccessControl"]="PublicRead"
-#s3bucketproperties["BucketName"]="skfgfhskh"
-#s3bucket["Properties"]=s3bucketproperties
-resources["HelloWorld"]=s3bucket
+    s3_bucket_type="AWS::S3::Bucket"
+    iam_user_type="AWS::IAM::User"
+    iam_group_type="AWS::IAM::Group"
 
-#print(stack)
-j = json.dumps(stack, indent=4)
-print(j)
+
+    stack=json.loads(template)
+
+
+    
+    body=json.loads(str(event["body"]))
+    properties=body["Properties"]
+    
+    resources=stack["Resources"] 
+    s3bucket={}
+    s3bucket.update({"Type" : s3_bucket_type,"Properties":{}})
+    
+    for property in body["Properties"]:
+        s3bucket["Properties"].update(
+         {property:body["Properties"][property]}## unsual jugaad but need to use it as I dont know the amount of properties that may come
+        )
+    #####for local testing
+    """bodytext='''{
+    "Properties" : {
+       "AccessControl" : "PublicRead",
+       "BucketName" : "dfdfdfdfdf",
+       "hello":"adfdfdf"           
+    }
+    }'''
+
+    body=json.loads(bodytext)"""
+
+    
+
+
+    resources.update({"HelloWorld":s3bucket})
+
+
+    #print(stack)
+    j = json.dumps(stack, indent=4)
+    print(j)
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
